6ed4475f038665e4df4ef48902ec178926b53645
Grace de la Mora <thetrend@users.noreply.github.com>
2023-02-24 21:53:11 -0500

    Add search form

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ const title = 'React';
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello {title}</h1>
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" />
     </div>
   );
 }
