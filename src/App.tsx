dc8e50cd03c4b9951ef31818865062dfd6316cd1
Grace de la Mora <thetrend@users.noreply.github.com>
2023-02-25 12:54:19 -0500

    Memoizing

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,7 @@ const App = () => {
     { data: [], isLoading: false, isError: false }
   );
 
-  React.useEffect(() => {
+  const handleFetchStories = React.useCallback(() => {
     if (!searchTerm) return;
 
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
@@ -76,6 +76,10 @@ const App = () => {
       .catch(() =>
         dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
   }, [searchTerm]);
+
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
 
   const handleRemoveStory = item => {
     dispatchStories({
