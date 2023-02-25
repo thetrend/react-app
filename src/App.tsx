53987711e2ab36873503c0d4e8565991bef595bb
Grace de la Mora <thetrend@users.noreply.github.com>
2023-02-24 22:19:37 -0500

    React Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
+// @ts-nocheck
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const handleChange = event => {
+    console.log(event.target.value);
+  }
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
-      <input type="text" id="search" />
-      {/* @ts-ignore */}
-      <List />
+      <input type="text" id="search" onChange={handleChange} />
+      <List list={stories} />
     </div>
   );
 }
