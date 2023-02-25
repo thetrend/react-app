9db31ac9d9f3c3ae2fa84945e604e3818666d521
Grace de la Mora <thetrend@users.noreply.github.com>
2023-02-25 13:29:54 -0500

    Add styles

--- src/App.tsx
+++ src/App.tsx
@@ -106,8 +106,8 @@ const App = () => {
   );
 
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <div className="container">
+      <h1 className="headline-primary">My Hacker Stories</h1>
 
       <SearchForm
         searchTerm={searchTerm}
@@ -131,7 +131,7 @@ const SearchForm = ({
   onSearchInput,
   onSearchSubmit,
 }) => (
-  <form onSubmit={onSearchSubmit}>
+  <form onSubmit={onSearchSubmit} className="search-form">
     <InputWithLabel
       id="search"
       label="Search"
@@ -140,7 +140,11 @@ const SearchForm = ({
     >
       <strong>Search:</strong>
     </InputWithLabel>
-    <button type="submit" disabled={!searchTerm}>
+    <button
+      type="submit"
+      disabled={!searchTerm}
+      className="button button_large"
+    >
       Submit
     </button>
   </form>
@@ -164,8 +168,8 @@ const InputWithLabel = ({
 
   return (
     <>
-      <label htmlFor={id}>{children}</label>
-      <input ref={inputRef} id={id} type={type} value={value} onChange={onInputChange} />
+      <label htmlFor={id} className="label">{children}</label>
+      <input className="input" ref={inputRef} id={id} type={type} value={value} onChange={onInputChange} />
     </>
   );
 };
@@ -183,13 +187,19 @@ const Item = ({ item, onRemoveItem }) => {
   const handleRemoveItem = () => onRemoveItem(item);
 
   return (
-    <div key={item.objectID}>
-      <span><a href={item.url}>{item.title}</a></span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-      <span>
-        <button type="button" onClick={handleRemoveItem}>
+    <div className="item" key={item.objectID}>
+      <span style={{ width: '40%' }}>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span style={{ width: '30%' }}>{item.author}</span>
+      <span style={{ width: '10%' }}>{item.num_comments}</span>
+      <span style={{ width: '10%' }}>{item.points}</span>
+      <span style={{ width: '10%' }}>
+        <button 
+          type="button"
+          onClick={handleRemoveItem}
+          className="button button_small"
+        >
           Dismiss
         </button>
       </span>
