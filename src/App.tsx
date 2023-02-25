7b0bc5ba8c04f8a1c8eb542ecbffc831956c8c1a
Grace de la Mora <thetrend@users.noreply.github.com>
2023-02-24 22:01:23 -0500

    Display list items

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
 import './App.css';
 
-function getTitle(title: string) {
-  return title;
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello {getTitle('React')}</h1>
+      <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
       <input type="text" id="search" />
+      {list.map(function(item) {
+        return (
+          <div key={item.objectID}>
+            <span><a href={item.url}>{item.title}</a></span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        );
+      })}
     </div>
   );
 }
