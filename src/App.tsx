cf6a5149123b23ee1e88c0ca3481355194462e12
Grace de la Mora <thetrend@users.noreply.github.com>
2023-02-24 21:55:53 -0500

    Add a function as an expression

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import './App.css';
 
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
-};
+function getTitle(title: string) {
+  return title;
+}
 
 function App() {
   return (
     <div>
-      <h1>{welcome.greeting} {welcome.title}</h1>
+      <h1>Hello {getTitle('React')}</h1>
       <label htmlFor="search">Search: </label>
       <input type="text" id="search" />
     </div>
