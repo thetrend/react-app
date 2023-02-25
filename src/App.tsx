690ea0e52dc6e03bb66877bca40c1a7e397898ba
Grace de la Mora <thetrend@users.noreply.github.com>
2023-02-24 22:12:57 -0500

    Refactoring to arrow functions

--- src/App.tsx
+++ src/App.tsx
@@ -19,24 +19,27 @@ const list = [
   },
 ];
 
-function App() {
+const App = () => {
   return (
     <div>
       <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
       <input type="text" id="search" />
-      {list.map(function(item) {
-        return (
-          <div key={item.objectID}>
-            <span><a href={item.url}>{item.title}</a></span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        );
-      })}
+      {/* @ts-ignore */}
+      <List />
     </div>
   );
 }
+
+const List = () => {
+  return list.map(item => (
+    <div key={item.objectID}>
+      <span><a href={item.url}>{item.title}</a></span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  ));
+};
 
 export default App;
