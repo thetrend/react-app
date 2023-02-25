aba59c8aa22031cbf3637c4cc42c5d9f8d20e268
Grace de la Mora <thetrend@users.noreply.github.com>
2023-02-24 21:54:20 -0500

    Adding object

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
 
-const title = 'React';
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+};
 
 function App() {
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>{welcome.greeting} {welcome.title}</h1>
       <label htmlFor="search">Search: </label>
       <input type="text" id="search" />
     </div>
