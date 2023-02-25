db3b4cee084441e6212b0da2a4671d40a136ac70
Grace de la Mora <thetrend@users.noreply.github.com>
2023-02-25 12:51:43 -0500

    Implementing Data Fetching

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,7 @@
 import React from 'react';
 import './App.css';
 
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const getAsyncStories = () =>
-  new Promise(resolve =>
-    setTimeout(
-      () => resolve({ data: { stories: initialStories } }),
-      2000
-    )
-  );
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -86,17 +61,21 @@ const App = () => {
   );
 
   React.useEffect(() => {
+    if (!searchTerm) return;
+
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}${searchTerm}`)
+      .then(response => response.json())
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.stories,
-        });
+          payload: result.hits,
+        })
       })
-      .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
-  }, []);
+      .catch(() =>
+        dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
+  }, [searchTerm]);
 
   const handleRemoveStory = item => {
     dispatchStories({
